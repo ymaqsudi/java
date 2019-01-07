b56f8b2f7537b699f676ac0ae4f4cb89a3e41261
Yaseen Maqsudi <yaseenmaqsudi@gmail.com>
2019-01-07 18:52:20 -0500

    Number to Binary Converter

---
+++ Binary Converter/src/Converter.java
@@ -0,0 +1,30 @@
+
+public class Converter {
+
+	public static void main(String[] args) {
+		
+		// Divide the number by 2 until you get a quotient of 1
+		// Print the remainders of the numbers backwards (Starting from 1)
+		
+		// Assigning X to equal any number and creating an array with 1000 units
+		int x = 10;
+		int binaryNum[] = new int[10];
+		
+		
+		// Getting the remainder for each number
+		int i = 0;
+		while (x > 0) {
+			binaryNum[i] = x % 2;
+			x = x / 2;
+			i++;
+			
+		}
+		
+		// Printing the remainders in the array in backwards order
+		for (int z = i - 1; z >= 0; z--) 
+            System.out.print(binaryNum[z]);
+		
+
+	}
+
+}
